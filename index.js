b40968500f0fe4676686c9b87aed6f0b2dee04ed
Kristen Picard <kristenpicard93@gmail.com>
2021-10-05 16:00:16 -0500

    Making more functions pure

--- index.js
+++ index.js
@@ -8,8 +8,83 @@ let one = "hidden";
 let two = "hidden";
 let three = "hidden";
 
+// pure function
+function waitInSeconds(func, n) {
+  setTimeout(func, n * 1000);
+}
+
+// pure function
+function isDone(button1, button2, button3) {
+  if (
+    button1.innerHTML != "" &&
+    button2.innerHTML != "" &&
+    button3.innerHTML != ""
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// pure function
+function shouldShow(button) {
+  if (button.id == "btn3" && (two == "hidden" || one == "hidden")) {
+    return false;
+  } else if (button.id == "btn2" && one == "hidden") {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function showNumber(button) {
+  if (button.id == "btn1") {
+    button1.innerHTML = "1";
+    one = "shown";
+  } else if (button.id == "btn2") {
+    button2.innerHTML = "2";
+    two = "shown";
+  } else if (button.id == "btn3") {
+    button3.innerHTML = "3";
+    three = "shown";
+  }
+}
+
+function hideNumber(button) {
+  if (button.id == "btn1") {
+    button1.innerHTML = "";
+    one = "hidden";
+  } else if (button.id == "btn2") {
+    button2.innerHTML = "";
+    two = "hidden";
+  } else if (button.id == "btn3") {
+    button3.innerHTML = "";
+    three = "hidden";
+  }
+}
+
+// What happens upon clicking either boxes or reset
+function onClick(event) {
+  let clickedButton = event.target;
+
+  showNumber(clickedButton);
+
+  waitInSeconds(function () {
+    if (shouldShow(clickedButton)) {
+      // do nothing
+    } else {
+      console.log("test");
+      hideNumber(clickedButton);
+    }
+  }, 1);
+
+  if (isDone(button1, button2, button3)) {
+    winMessage.innerHTML = "You Win!!!";
+  }
+}
+
 // Empties boxes and takes away You Win message
-function resetBoard() {
+function onClickReset() {
   button1.innerHTML = "";
   button2.innerHTML = "";
   button3.innerHTML = "";
@@ -19,70 +94,7 @@ function resetBoard() {
   three = "hidden";
 }
 
-// Shows the number "behind" the card clicked and calls wait sec func
-function showNumber(button) {
-  if (button.id == "btn1") {
-    button1.innerHTML = "1";
-    one = "shown";
-  } else if (button.id == "btn2") {
-    button2.innerHTML = "2";
-  } else if (button.id == "btn3") {
-    button3.innerHTML = "3";
-  }
-  waitOneSec(button);
-}
-
-// Waits 1 second before calling check if the number should stay showing
-function waitOneSec(button) {
-  setTimeout(function () {
-    checkIfShouldStayUp(button);
-  }, 1000);
-}
-
-// If all three buttons filled, displays win message
-function checkIfDone() {
-  if (
-    button1.innerHTML != "" &&
-    button2.innerHTML != "" &&
-    button3.innerHTML != ""
-  ) {
-    winMessage.innerHTML = "You Win!!!";
-  }
-}
-
-// Checks chosen button in relation to other buttons. Changes the inner HTML if needed AND changes status.
-function checkIfShouldStayUp(button) {
-  if (one == "hidden" && button.id == "btn2") {
-    button2.innerHTML = "";
-  } else if (one == "shown" && button.id == "btn2") {
-    two = "shown";
-  } else if (one == "hidden" && button.id == "btn3") {
-    button3.innerHTML = "";
-  } else if (one == "shown" && two == "hidden" && button.id == "btn3") {
-    button3.innerHTML = "";
-  } else if (one == "shown" && two == "shown" && button.id == "btn3") {
-    button3.innerHTML = "3";
-    three = "shown";
-  }
-}
-
-// What happens upon clicking either boxes or reset
-function onClick(event) {
-  let clickedButton = event.target;
-
-  // If they click a box, this is called
-  showNumber(clickedButton);
-
-  // Called after each click
-  checkIfDone(clickedButton);
-
-  // If click reset button, calls resetBoard function
-  if (clickedButton == reset) {
-    resetBoard();
-  }
-}
-
 button1.addEventListener("click", onClick);
 button2.addEventListener("click", onClick);
 button3.addEventListener("click", onClick);
-reset.addEventListener("click", onClick);
+reset.addEventListener("click", onClickReset);
